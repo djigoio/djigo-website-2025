2b221d926407c6dd126d9bbee982fc6c83f71e05
Antonio Djigo <adjigo@Antonios-MacBook-Air.local>
2024-11-25 15:43:20 +0000

    desktop fix

--- app/components/CS.tsx
+++ app/components/CS.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Item from './Item'
 
 const CS = () => {
-    return (<div className='text-right customer-content' style={{ wordWrap: 'break-word', width: '65%',  marginLeft:'auto', paddingRight: '20px', marginTop:'40px'}}>
+    return (<div className='text-right customer-content' style={{ wordWrap: 'break-word', width: '65%',  marginLeft:'auto', paddingRight: '20px', marginTop:'auto'}}>
     <Item text="Manage *full life-cycle* of customer relationships."/>
     <Item text="Onboarding, configuration, training, best practices."  />
     <Item text="Coordinate *technical support*, manage escalations."  />
